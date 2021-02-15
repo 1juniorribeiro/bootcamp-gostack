0754223a60fcc9731d88a3c85c03e5a984143bbf
Amauri Junior <junior_18ribeiro@hotmail.com>
2021-02-15 09:02:40 -0400

    struct toast message

--- 06-gobarber-web/src/App.tsx
+++ 06-gobarber-web/src/App.tsx
@@ -4,6 +4,7 @@ import Globalstyle from './styles/global';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUP';
 
+import ToastContainer from './components/ToastContainer';
 import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
@@ -11,6 +12,8 @@ const App: React.FC = () => (
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer />
 
     <Globalstyle />
   </>
