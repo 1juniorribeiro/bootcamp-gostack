ddf8e41bccb194169f6d045d918eed0a636be34c
Amauri Junior <junior_18ribeiro@hotmail.com>
2021-02-15 17:28:22 -0400

    routes authenticated

--- 06-gobarber-web/src/routes/index.tsx
+++ 06-gobarber-web/src/routes/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import signIn from '../pages/SignIn';
 import signUp from '../pages/SignUp';
+
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={signIn} />
     <Route path="/signup" component={signUp} />
+
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
