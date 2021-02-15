ebce2624ff300042ef242a6adbf385e4483f0406
Amauri Junior <junior_18ribeiro@hotmail.com>
2021-02-15 08:23:15 -0400

    authenticate finished

--- 06-gobarber-web/src/App.tsx
+++ 06-gobarber-web/src/App.tsx
@@ -4,7 +4,7 @@ import Globalstyle from './styles/global';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUP';
 
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
