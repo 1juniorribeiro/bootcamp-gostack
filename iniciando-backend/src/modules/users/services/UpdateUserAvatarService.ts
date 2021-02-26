553ab6e1c08830861f398a71533e76a0252a3570
Amauri Junior <junior_18ribeiro@hotmail.com>
2021-02-26 16:49:35 -0400

    refactory users module

--- iniciando-backend/src/modules/users/services/UpdateUserAvatarService.ts
+++ iniciando-backend/src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,5 +1,4 @@
 /* eslint-disable camelcase */
-import { getRepository } from 'typeorm';
 import path from 'path';
 import fs from 'fs';
 
@@ -7,17 +6,18 @@ import uploadConfig from '@config/upload';
 import User from '@modules/users/infra/typeorm/entities/user';
 
 import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
 
-interface Request {
+interface IRequest {
   user_id: string;
   avatarFilename: string;
 }
 
 class UpdateUserAvatarService {
-  public async execute({ user_id, avatarFilename }: Request): Promise<User> {
-    const usersRepository = getRepository(User);
+  constructor(private usersRepository: IUsersRepository) {}
 
-    const user = await usersRepository.findOne(user_id);
+  public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
 
     if (!user) {
       throw new AppError('Only authenticated users can change avatar.', 401);
@@ -36,7 +36,7 @@ class UpdateUserAvatarService {
 
     user.avatar = avatarFilename;
 
-    await usersRepository.save(user);
+    await this.usersRepository.save(user);
 
     return user;
   }
