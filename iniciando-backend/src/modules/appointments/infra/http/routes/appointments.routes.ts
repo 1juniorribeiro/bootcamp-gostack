b5baf9986ebe5d5149111edf5a5559aa31c6ab2f
Amauri Junior <junior_18ribeiro@hotmail.com>
2021-02-26 16:06:11 -0400

    Dependency Inversion Principle

--- iniciando-backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ iniciando-backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,7 +1,6 @@
 /* eslint-disable camelcase */
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
 
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
@@ -9,21 +8,23 @@ import CreateAppointmentService from '@modules/appointments/services/CreateAppoi
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.get('/', async (request, response) => {
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-  const appointments = await appointmentsRepository.find();
+// appointmentsRouter.get('/', async (request, response) => {
+//   const appointments = await appointmentsRepository.find();
 
-  return response.json(appointments);
-});
+//   return response.json(appointments);
+// });
 
 appointmentsRouter.post('/', async (request, response) => {
   const { provider_id, date } = request.body;
 
   const parsedDate = parseISO(date);
-  const CreateAppointment = new CreateAppointmentService();
+  const CreateAppointment = new CreateAppointmentService(
+    appointmentsRepository,
+  );
 
   const appointment = await CreateAppointment.execute({
     provider_id,
